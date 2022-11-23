c2c1dc9c3770986595bc22228ddea8182c2bf5f6
Ethan <ereynolds@smxtech.com>
2022-11-23 15:31:41 -0500

    update launch configuration

--- py/main.py
+++ py/main.py
@@ -4,10 +4,6 @@ from mach.lex import *
 from mach.ast import *
 
 def main():
-    # read file example/zero/main.mach
-    src = open("example/zero/main.mach").read()
-    lex = Lex(src)
-
     if len(sys.argv) != 2:
         print("usage: mach <file>")
         sys.exit(1)
