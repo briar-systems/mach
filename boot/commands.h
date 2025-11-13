74a57072ec6850e88ae0dafbc81b6bf53a553081
octalide <ethayng@gmail.com>
2025-11-13 10:55:06 -0500

    fix: rename mach_cmd_new to mach_cmd_init and add mach_cmd_run

--- boot/commands.h
+++ boot/commands.h
@@ -2,8 +2,9 @@
 #define COMMANDS_H
 
 // minimal command entrypoints
+int  mach_cmd_init(int argc, char **argv);
 int  mach_cmd_build(int argc, char **argv);
-int  mach_cmd_new(int argc, char **argv);
+int  mach_cmd_run(int argc, char **argv);
 void mach_print_usage(const char *program_name);
 
 #endif
